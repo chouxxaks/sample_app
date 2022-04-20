1906e96f51718ce2bf498772abd4aee8934fc0ed
Ubuntu <ubuntu@ip-172-31-95-78.ec2.internal>
2022-04-20 08:22:57 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger
   end
+  
+  def new
+    @user = User.new
+    #debugger
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save       # 保存が成功したら
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # 別ページにリダイレクト「redirect_to user_url(@user)」と同じ
+      # redirect_to user_url(@user)
+    else                # 保存が失敗したら
+      render 'new'      # ？
+    end
+  end
+  
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
